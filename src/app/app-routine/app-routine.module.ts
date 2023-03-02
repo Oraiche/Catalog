72816882ad55d012393ac4d5a439235cf6ec6786
Mohammed <mohammed.oraiche@gmail.com>
2023-03-02 22:48:23 +0000

    initial commit

---
+++ src/app/app-routine/app-routine.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutineRoutingModule } from './app-routine-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutineRoutingModule
+  ]
+})
+export class AppRoutineModule { }
